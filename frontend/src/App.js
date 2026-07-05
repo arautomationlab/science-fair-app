99cc66c695ce03a6134a14d038caa7847e8dd0e3
Shaikh A. Raheman <raheman.maths47@gmail.com>
2026-07-05 19:54:11 +0530

    Trigger new deployment with correct settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -104,7 +104,7 @@ function App() {
               <RoleRoute allowedRoles={['admin']}>
                 <AdminDashboard />
               </RoleRoute>
-            } />
+             } />
             
             <Route path="/admin/winners/:grade" element={
               <RoleRoute allowedRoles={['admin']}>
